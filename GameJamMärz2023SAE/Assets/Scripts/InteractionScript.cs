bb857443f40d26dd72026fc873de1ed4a043b000
LauchsuppeSan <guilia.stephan.gs@gmail.com>
2023-03-04 00:47:32 +0100

    raycast

---
+++ GameJamMärz2023SAE/Assets/Scripts/InteractionScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionScript : MonoBehaviour
+{
+    [SerializeField] private GameObject InteractPanel;
+
+    private void Update()
+    {
+        Raycast();
+    }
+    public void Raycast()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+        Debug.DrawRay(transform.position, Vector3.forward, Color.red, 8);
+
+        bool hitSomething = Physics.Raycast(ray, out hit, 4);
+        Debug.Log(hitSomething);
+
+        if (hitSomething && hit.transform.CompareTag("Collectables"))
+        {
+            InteractPanel.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+
+            }
+        }
+        else
+        {
+            InteractPanel.SetActive(false);
+        }
+
+
+    }
+}
