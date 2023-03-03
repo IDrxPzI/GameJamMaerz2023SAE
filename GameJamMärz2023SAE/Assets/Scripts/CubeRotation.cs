0d5dbffb996e498de4a0ba9a2798e4129e862fd8
IDrxPzI <stefan.mench@web.de>
2023-03-03 20:22:33 +0100

    test2

--- GameJamMärz2023SAE/Assets/Scripts/CubeRotation.cs
+++ GameJamMärz2023SAE/Assets/Scripts/CubeRotation.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 
 public class CubeRotation : MonoBehaviour
 {
-    public float speed;
-
+    [SerializeField] private float rotateSpeed;
+    [SerializeField] private float freq;
+    [SerializeField] private float amp;
     private float frequency;
 
     private void Update()
     {
-        transform.Rotate(0, speed * Time.deltaTime, 0);
-       //transform.position = MathF.Sin();
+        var yMovement = Mathf.Sin(Time.time * freq) * amp;
+        transform.Translate(0, yMovement * Time.deltaTime, 0);
+
+        transform.Rotate(0, 1f * rotateSpeed, 0);
     }
 }
